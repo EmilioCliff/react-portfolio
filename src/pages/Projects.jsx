8630ba95be15d95afe665b8227b3edc765a4de41
EmilioCliff <emiliocliff@gmail.com>
2025-05-26 23:44:50 +0300

    refactor Projects component to use image URLs instead of local imports

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,4 @@
 import React, { useState } from 'react';
-// import apalisLogo from '../assets/apalis.webp';
-import ensafoImg from '../assets/ensafo.png';
-import collageCLIImg from '../assets/collageCLI.png';
-import andySafaricImg from '../assets/andySafaris.png';
-import inventoryImg from '../assets/inventorySystem.png';
-import paymentPollingImg from '../assets/payment-polling.png';
-import distributionImg from '../assets/distribution-management.png';
-import kokomedFinanceImg from '../assets/fron.png';
-import kokomedFinanceObserv from '../assets/prom.png';
-import myhelaAfricaImg from '../assets/myhela-africa.png';
-import mysql2S3Img from '../assets/railways_logs.png';
-import igniteImg from '../assets/ignite_help.png';
-import boffoImg from '../assets/boffo.png';
 import '../styles/project.css';
 
 const projectsData = [
@@ -20,7 +7,8 @@ const projectsData = [
 		title: 'Kokomed Finance',
 		description:
 			'A loan system for Afya Credit SACCO that handles applications, approvals, repayments, and user onboarding — processing Ksh 1M+ in its first 3 months.',
-		image: kokomedFinanceImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Ffron.png?alt=media&token=47480931-2bfa-49c4-aa6b-c96e602b007b',
 		category: 'Fullstack',
 		link: 'https://drive.google.com/file/d/16e2V0yx4Ds-MpdD4z_91fI8aC9r9RF0n/view',
 		tools: ['Go', 'React', 'MySQL'],
@@ -30,7 +18,8 @@ const projectsData = [
 		title: 'Kokomed Finance - Observability',
 		description:
 			'Built an observability layer for kokomed-finance using the Grafana ecosystem.',
-		image: kokomedFinanceObserv,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fprom.png?alt=media&token=54f049c3-9994-48f1-ab3f-c23293af93dd',
 		category: 'Tools',
 		link: 'https://drive.google.com/file/d/1T6uq9OJVTXoyt8rUZwQkJxJfz_IBXBG_/view',
 		tools: [
@@ -47,7 +36,8 @@ const projectsData = [
 		title: 'Ignite CLI',
 		description:
 			'Sets up Go projects with Hexagonal Architecture by generating boilerplate code and wiring for clean, modular design.',
-		image: igniteImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fignite_help.png?alt=media&token=a3e638a9-2db5-464f-b411-0c551869e7e6',
 		category: 'CLI',
 		link: 'https://drive.google.com/file/d/1KJp5tLep1m-UakTRTE-lxX9KPS95AIxx/view',
 		tools: ['Go', 'Hexagonal'],
@@ -57,7 +47,8 @@ const projectsData = [
 		title: 'mysql-r2-backups',
 		description:
 			'Automates MySQL backups on Railway and uploads them to Cloudflare R2 for easy, reliable cloud storage.',
-		image: mysql2S3Img,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Frailways_logs.png?alt=media&token=7bb0b677-7145-480c-825e-ddc570ee19d7',
 		category: 'Tools',
 		link: 'https://github.com/EmilioCliff/mysql-r2-backups',
 		tools: ['Go', 'MySQL', 'S3', 'Railway'],
@@ -67,7 +58,8 @@ const projectsData = [
 		title: 'MyHela Africa',
 		description:
 			'An AI-powered personal finance tracker that analyzes M-Pesa statements and allows users to ask questions in natural language. It gives insights like spending trends, balances, and financial tips.',
-		image: myhelaAfricaImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fmyhela-africa.png?alt=media&token=29a30fec-1334-451f-84b2-9e3ddca0766d',
 		category: 'Fullstack',
 		link: 'https://myhela.africa/',
 		tools: ['React', 'Django', 'PostgreSQL', 'OpenAI'],
@@ -77,7 +69,8 @@ const projectsData = [
 		title: 'Payment Polling Service',
 		description:
 			'A Go microservice built for a backend assessment that polls payment APIs and shares transaction statuses via RabbitMQ and gRPC.',
-		image: paymentPollingImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fpayment-polling.png?alt=media&token=0228ef91-3a1c-4665-8ae0-e88de9e074cc',
 		category: 'Backend',
 		link: 'https://github.com/EmilioCliff/payment-polling-service',
 		tools: ['Microservice', 'Go', 'SwaggerAPI'],
@@ -87,7 +80,8 @@ const projectsData = [
 		title: 'Inventory Club Management System',
 		description:
 			'A Python Flask app for managing club inventory with a clean UI and efficient tracking of stock and employees.',
-		image: inventoryImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2FinventorySystem.png?alt=media&token=2bbc5cfc-9bac-47ca-ae46-b4a8ae37ec1e',
 		category: 'Fullstack',
 		link: 'https://youtu.be/ez4a6alwvbY',
 		tools: ['Flask', 'Bootstrap5', 'SQLite'],
@@ -97,7 +91,8 @@ const projectsData = [
 		title: 'Collage CLI',
 		description:
 			'A terminal-based student management system for handling students, courses, and semesters with a focus on speed and simplicity.',
-		image: collageCLIImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2FcollageCLI.png?alt=media&token=64e0faf2-3c20-4e2f-95ab-90d9d8b6115e',
 		category: 'CLI',
 		link: 'https://github.com/EmilioCliff/collage-cli',
 		tools: ['Go', 'SQLite', 'PromptUI', 'Cobra'],
@@ -107,7 +102,8 @@ const projectsData = [
 		title: 'Andy Safaris Kenya',
 		description:
 			"A travel site showcasing Andy Safaris Kenyan's, safari packages, booking features, and engaging content.",
-		image: andySafaricImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2FandySafaris.png?alt=media&token=93748a7a-bde8-4243-89b2-efbf5fb1246e',
 		category: 'Website',
 		link: 'https://andysafaris.com',
 		tools: ['HTML', 'CSS', 'JavaScript'],
@@ -117,7 +113,8 @@ const projectsData = [
 		title: 'Ensafo Ltd',
 		description:
 			"Ensafo Ltd's corporate website, designed for an engaging browsing experience.",
-		image: ensafoImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fensafo.png?alt=media&token=6862b93a-e457-4891-a6b1-5713c2853801',
 		category: 'Website',
 		link: 'https://ensafo.vercel.app',
 		tools: ['HTML', 'CSS', 'JavaScript'],
@@ -127,7 +124,8 @@ const projectsData = [
 		title: 'PharmaTrack',
 		description:
 			'A pharmaceutical distribution system that manages sales reps, generatre invoices and tracks their sales, streamlining operations and business insights.',
-		image: distributionImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fdistribution-management.png?alt=media&token=1db32c80-0936-47a9-8fc5-ebd7a390bbf5',
 		category: 'Fullstack',
 		link: 'https://youtu.be/Y5Xl81zUBy8',
 		tools: ['Go', 'Flask', 'PostgreSQL'],
@@ -137,7 +135,8 @@ const projectsData = [
 		title: 'Boffo Diapers Kenya',
 		description:
 			'A proposal for Boffo, a Kenyan baby diapers company, showcasing a potential website design with product highlights—currently with placeholder content and missing images.',
-		image: boffoImg,
+		imageUrl:
+			'https://firebasestorage.googleapis.com/v0/b/react-portfolio-fb429.appspot.com/o/projects%2Fboffo.png?alt=media&token=056836c5-1f26-4196-8a73-5383b796b815',
 		category: 'Website',
 		link: 'https://boffo.vercel.app/',
 		tools: ['React', 'v0.dev'],
@@ -181,7 +180,7 @@ function Projects() {
 				{filteredProjects.map((project) => (
 					<div key={project.id} className="project-card">
 						<img
-							src={project.image}
+							src={project.imageUrl}
 							alt={project.title}
 							style={{ objectFit: 'cover', height: '280px' }}
 							className="project-card__image"
